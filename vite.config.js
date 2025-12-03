4e63f45ae18e8c1826d0a056362013c287b667df
Mangalmurti Wagh <mangalmurtiwagh51@gmail.com>
2025-12-03 20:39:12 +0530

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/NewPortfolio/'   // <repo-name> ko aapke GitHub repo name se replace karo
-})
-
